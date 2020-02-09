94de35672a7d859a370afb9d9a82f7f3a05513e8
Florian Ipate <florianipate@Florians-iMac.local>
2020-02-09 22:34:09 +0000

    Updates

---
+++ precache-manifest.44cf800ce444b800b262a7fe3a71442d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff6a678a4fa62085578a27e1654bbb0",
+    "url": "/the-monsters/index.html"
+  },
+  {
+    "revision": "ed10d8f9cf854fbc1351",
+    "url": "/the-monsters/static/css/main.8a91ac1a.chunk.css"
+  },
+  {
+    "revision": "61afb2c62f9a09c48192",
+    "url": "/the-monsters/static/js/2.f7c812f5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/the-monsters/static/js/2.f7c812f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed10d8f9cf854fbc1351",
+    "url": "/the-monsters/static/js/main.6306cb49.chunk.js"
+  },
+  {
+    "revision": "35e4b45a9fa2183606ef",
+    "url": "/the-monsters/static/js/runtime-main.b980a68b.js"
+  }
+]);
